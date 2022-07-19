538b8e98213b783efc64c1ddfac787b56fa18e8f
Charles Jarvis <csjarvis43@gmail.com>
2022-07-19 15:31:18 -0600

    commit tree f1d5a55335eb95f46972501a9e87430643160734

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+require 'pry'
+
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble
+        @condition="new"
+        puts ("Your shoe is as good as new!")
+    end
+end
+
+# s1 = Shoe.new("nike")
+# s1.condition = "old"
+# binding.pry
+# s1.cobble
+# binding.pry
